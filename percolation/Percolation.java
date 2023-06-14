c9816aadc69bef9770661a7d9d5372f7c98ec881
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-06-14 11:35:50 +0530

    Defined Percolation model but not implemented.

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -4,10 +4,37 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
-import edu.princeton.cs.algs4.StdOut;
-
 public class Percolation {
+    public Percolation(int n) {
+        // Create 2D array.
+        int[][] gird = new int[n][n];
+    }
+
+    public void open(int row, int col) {
+        // TODO
+    }
+
+    public boolean isOpen(int row, int col) {
+        // TODO
+        return false;
+    }
+
+    public boolean isFull(int row, int col) {
+        // TODO
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        // TODO
+        return 0;
+    }
+
+    public boolean percolates() {
+        // TODO
+        return false;
+    }
+
     public static void main(String[] args) {
-        StdOut.println("Hello, world!");
+        // TODO TEST Client
     }
 }
