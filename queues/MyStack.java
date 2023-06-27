c1c59eeba19d9eec07da9a9aa8ea5cfbeaf7aab8
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-06-27 10:56:28 +0530

    Add iteration functionality to stack

--- queues/MyStack.java
+++ queues/MyStack.java
@@ -1,6 +1,7 @@
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class MyStack<T> {
+public class MyStack<T> implements Iterable<T> {
 
     private Node<T> head = null;
 
@@ -29,11 +30,46 @@ public class MyStack<T> {
         return item;
     }
 
+
+    // Iterator
+    public Iterator<T> iterator() {
+        return new StackIterator();
+    }
+
+    private class StackIterator implements Iterator<T> {
+        private Node<T> current = head;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public T next() {
+            if (hasNext()) {
+                T item = current.item;
+                current = current.next;
+                return item;
+            }
+            else {
+                throw new NoSuchElementException("Stack is empty!");
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         // Test client
-        MyStack<Integer> stack = new MyStack<Integer>();
-        stack.push(12);
-        stack.push(13);
-        stack.push(14);
+        // MyStack<Integer> stack = new MyStack<Integer>();
+        // stack.push(12);
+        // stack.push(13);
+        // stack.push(14);
+        //
+        // MyStack<String> names = new MyStack<String>();
+        // names.push("Harry");
+        // names.push("Hermione");
+        // names.push("Ron");
+        //
+        // for (String name : names)
+        //     StdOut.println(name);
+
     }
 }
